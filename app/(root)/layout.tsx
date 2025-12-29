971e6349be1fc3ebfd47353c4450f0b70ed43d07
Sudhanshu S. Sindhu <Sudhanshu@Mac.fritz.box>
2025-12-29 02:50:54 +0100

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+const Layout = ({ children}: {children : React.ReactNode}) => {
+    return (
+        <main className="min-h-screen text-gray-400">
+            <div className="container py-10">
+                {children}
+            </div>
+        </main>
+    )
+}
+export default Layout
